8be477e803b8582cb2b81d56dc21cfc37dffe4ff
Ahmed-Nakhuda <144385153+Ahmed-Nakhuda@users.noreply.github.com>
2024-05-11 16:44:15 -0400

    Add files via upload

---
+++ places.js
@@ -0,0 +1,131 @@
+/* 
+    "StAuth10244: I Ahmed Nakhuda, 000878456 certify that this material is my original work. 
+    No other person's work has been used without due acknowledgement. 
+    I have not made my work available to anyone else."
+*/ 
+const places = [
+
+    // Attractions
+{
+    "LATITUDE": 43.0906,
+    "LONGITUDE": -79.0759,
+    "NAME": "Dinosaur Adventure Golf",
+    "ADDRESS": "4952 Clifton Hill, Niagara Falls, ON L2G 3N4",
+    "CATEGORY": "Attraction"
+    
+},
+{
+    "LATITUDE": 43.0911,
+    "LONGITUDE": -79.0746,
+    "NAME": "Great Canadian Midway Arcade",
+    "ADDRESS": "4960 Clifton Hill, Niagara Falls, ON L2G 3N4",
+    "CATEGORY": "Attraction"
+},
+{
+    "LATITUDE": 43.0909,
+    "LONGITUDE": -79.0755,
+    "NAME": "Niagara SkyWheel",
+    "ADDRESS": "4848 Clifton Hill, Niagara Falls, ON L2G 3N4",
+    "CATEGORY": "Attraction"
+   
+},
+{
+    "LATITUDE": 43.0851,
+    "LONGITUDE": -79.0796,
+    "NAME": "Skylon Tower",
+    "ADDRESS": "5200 Robinson St, Niagara Falls, ON L2G 2A2",
+    "CATEGORY": "Attraction"
+},
+{
+    "LATITUDE": 43.1186,
+    "LONGITUDE": -79.0744,
+    "NAME": "Niagara Helicopters",
+    "ADDRESS": "3731 Victoria Ave, Niagara Falls, ON L2E 6V5",
+    "CATEGORY": "Attraction"
+},
+
+
+
+
+//Museums 
+{
+    "LATITUDE": 43.1555,
+    "LONGITUDE": -79.1945,
+    "NAME": "St. Cathrines Museum",
+    "ADDRESS": "1932 Welland Canals Parkway, St. Catharines, ON L2R 7K6",
+    "CATEGORY": "Museum"
+},
+{
+    "LATITUDE": 43.0894816,
+    "LONGITUDE": -79.0911801,
+    "NAME": "Niagara Falls History Museum",
+    "ADDRESS": "5810 Ferry St, Niagara Falls, ON L2G 1S9",
+    "CATEGORY": "Museum"
+},
+{
+    "LATITUDE": 43.101234,
+    "LONGITUDE":  -79.073079,
+    "NAME": "Niagara Military Museum",
+    "ADDRESS": "5049 Victoria Ave, Niagara Falls, ON L2E 4E2",
+    "CATEGORY": "Museum"
+},
+
+
+
+    // Outdoor Recreation 
+{
+    "LATITUDE":  43.135132,
+    "LONGITUDE": -79.054437,
+    "NAME": "Niagara Park Botanical Gardens",
+    "ADDRESS": "2565 Niagara River Pkwy, Niagara Falls, ON L2E 2S7",
+    "CATEGORY": "Outdoor Recreation"
+},
+{
+    "LATITUDE": 43.151807,
+    "LONGITUDE":  -79.048213,
+    "NAME": "Centennial Lilac Garden",
+    "ADDRESS": "14004 Niagara River Pkwy, Niagara Falls, ON",
+    "CATEGORY": "Outdoor Recreation"
+},
+{
+    "LATITUDE": 43.0687,
+    "LONGITUDE":  -79.0707,
+    "NAME": "Dufferin Islands",
+    "ADDRESS": "6345 Dufferin Isle Rd, Niagara Falls, ON L0S 1A0",
+    "CATEGORY": "Outdoor Recreation"
+},
+
+
+
+
+    // Hotels
+{
+    "LATITUDE": 43.0911,
+    "LONGITUDE": -79.0727,
+    "NAME": "Sheraton Fallsview Hotel",
+    "ADDRESS": "5875 Falls Ave, Niagara Falls, ON L2G 3K7",
+    "CATEGORY": "Hotel"
+},
+{
+    "LATITUDE": 43.09865,
+    "LONGITUDE":  -79.06595,
+    "NAME": "Redwood Bed & Breakfast",
+    "ADDRESS": "5227 River Rd, Niagara Falls, ON L2E 3G9",
+    "CATEGORY": "Hotel"
+},
+{
+    "LATITUDE": 43.0796,
+    "LONGITUDE": -79.0825,
+    "NAME": "Embassy Suites by Hilton Niagara Falls Fallsview",
+    "ADDRESS": "6700 Fallsview Blvd, Niagara Falls, ON L2G 3W6",
+    "CATEGORY": "Hotel"
+},
+
+]
+
+
+
+
+
+
+ 
